71233170eff814ca40e144a48d27672921bb7827
Jan Morawietz <jan.morawietz@paessler.com>
2020-02-19 13:58:07 +0100

    switch pcs

--- src/CardModel.cs
+++ src/CardModel.cs
@@ -10,5 +10,25 @@ namespace Nancy.Simple
         public string rank { get; set; }
         public string suit { get; set; }
 
+        public int rankValue
+        {
+            get
+            {
+                switch (rank[0])
+                {
+                    case 'A':
+                        return 14;
+                    case 'K':
+                        return 13;
+                    case 'Q':
+                        return 12;
+                    case 'J':
+                        return 11;
+                    default:
+                        return int.Parse(rank);
+                }
+            }
+        }
+
     }
 }
--- src/PlayerModel.cs
+++ src/PlayerModel.cs
@@ -13,6 +13,43 @@ namespace Nancy.Simple
         public string version { get; set; }
         public int stack { get; set; }
         public int bet { get; set; }
+        public CardModel[] hole_cards { get; set; }
 
+        public int EvaluateHand()
+        {
+            var retVal = 0;
+
+            if (GotPair())
+                retVal += 2;
+            if (GotStraightStart())
+                retVal += 2;
+            if (GotSameSuit())
+                retVal += 1;
+            if (GotSomething())
+                retVal += 1;
+
+            return retVal;
+        }
+
+        private bool GotPair()
+        {
+            return hole_cards[0].rank == hole_cards[1].rank;
+        }
+
+
+        private bool GotStraightStart()
+        {
+            return Math.Abs(hole_cards[0].rankValue - hole_cards[1].rankValue) == 1;
+        }
+
+        private bool GotSameSuit()
+        {
+            return hole_cards[0].suit == hole_cards[1].suit;
+        }
+
+        private bool GotSomething()
+        {
+            return hole_cards[0].rankValue > 10 || hole_cards[1].rankValue > 10;
+        }
     }
 }
