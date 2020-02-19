4c0c3c988980e89da79786dd3e9e0f8cabbcb6d5
Jakob Leurs <Jakob.Leurs@gmx.de>
2020-02-19 10:56:00 +0100

    Minimum raise

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -11,13 +11,19 @@ namespace Nancy.Simple
             //var dGameState = (dynamic)gameState;
             //TODO: Use this method to return the value You want to bet
             //return (int)gameState["current_buy_in"] - (int)gameState["players"][gameState["in_action"]]["bet"] + (int)gameState["minimum_raise"];
-            int small_blind = (int)(gameState)["small_blind"];
-            return 50;
+            //int small_blind = (int)(gameState)["small_blind"];
+            return JakobsAlgorithm(gameState);
         }
 
         public static void ShowDown(JObject gameState)
         {
             //TODO: Use this method to showdown
         }
+
+		private static int JakobsAlgorithm(JObject gameState)
+		{
+			int minimum_raise = (int)gameState["minimum_raise"];
+			return minimum_raise;
+		}
     }
 }
