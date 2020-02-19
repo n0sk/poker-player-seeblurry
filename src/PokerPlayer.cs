8217cf544318b1990d044160cffb7d2fb525b0e2
Jan Morawietz <jan.morawietz@paessler.com>
2020-02-19 10:44:37 +0100

    revert

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -2,22 +2,24 @@
 
 namespace Nancy.Simple
 {
-    public static class PokerPlayer
-    {
-        public static readonly string VERSION = "Default C# folding player";
+	public static class PokerPlayer
+	{
+		public static readonly string VERSION = "Default C# folding player";
 
-        public static int BetRequest(JObject gameState)
+		public static int BetRequest(JObject gameState)
         {
-            var dGameState = (dynamic)gameState;
-            //TODO: Use this method to return the value You want to bet
-            return (int)gameState["current_buy_in"] - (int)gameState["player"][gameState["in_action"]]["bet"] + (int)gameState["minimum_raise"];
-            int small_blind = (int)(gameState)["small_blind"];
-            return 3 * small_blind;
+
+            var dGameState = (dynamic) gameState;
+			//TODO: Use this method to return the value You want to bet
+			//return (int)dGameState.current_buy_in - (int)dGameState.player.in_action.bet + (int)dGameState.minimum_raise;
+			int small_blind = (int)(gameState)["small_blind"];
+			return 3 * small_blind;
         }
 
-        public static void ShowDown(JObject gameState)
-        {
-            //TODO: Use this method to showdown
-        }
-    }
+		public static void ShowDown(JObject gameState)
+		{
+			//TODO: Use this method to showdown
+		}
+	}
 }
+
